5ffaccdf574eef64213069019758b1d609518f3b
Taylor Lilley <lilleyt09@gmail.com>
2021-04-26 16:02:29 -0400

    updated spacing

--- index.js
+++ index.js
@@ -3,11 +3,8 @@ const inquirer = require('inquirer');
 
 const connection = mysql.createConnection({
     host: 'localhost',
-
     port: 3306,
-
     user: 'root',
-
     password: '*orpheus1991*',
     database: 'company_db',
 });
